7337fd092047fa3ebed373072cafaacb9f91f771
Christoffer G. Thomsen <chris@cgt.name>
2017-01-01 18:49:17 +0100

    Expand tabs to 4 spaces

--- pyzor.lua
+++ pyzor.lua
@@ -35,34 +35,34 @@ local cfg_port = 5953
 --{"PV": "2.1", "Code": "200", "WL-Count": "0", "Count": "53", "Thread": "53416", "Diag": "OK"}
 
 local function check_pyzor(task)
-	local function cb(err, data)
-		if err then
-			logger.errx("request error: %s", err)
-			return
-		end
-		logger.debugm(N, task, 'data: %s', tostring(data))
+    local function cb(err, data)
+        if err then
+            logger.errx("request error: %s", err)
+            return
+        end
+        logger.debugm(N, task, 'data: %s', tostring(data))
 
-		local parser = ucl.parser()
-		local ok, err = parser:parse_string(tostring(data))
-		if not ok then
-			logger.errx("error parsing response: %s", err)
-			return
-		end
+        local parser = ucl.parser()
+        local ok, err = parser:parse_string(tostring(data))
+        if not ok then
+            logger.errx("error parsing response: %s", err)
+            return
+        end
 
-		local resp = parser:get_object()
-		local whitelisted = tonumber(resp["WL-Count"])
-		local reported = tonumber(resp["Count"])
+        local resp = parser:get_object()
+        local whitelisted = tonumber(resp["WL-Count"])
+        local reported = tonumber(resp["Count"])
 
-		logger.infox("count=%s wl=%s", reported, whitelisted)
+        logger.infox("count=%s wl=%s", reported, whitelisted)
 
-		-- Make whitelists count a little bit.
-		-- Maybe there's a better way to take whitelists into account,
-		-- but at least this is something.
-		reported = reported - whitelisted
+        -- Make whitelists count a little bit.
+        -- Maybe there's a better way to take whitelists into account,
+        -- but at least this is something.
+        reported = reported - whitelisted
 
         local weight = 0
 
-		if reported >= 100 then
+        if reported >= 100 then
             weight = 1.5
         elseif reported >= 25 then
             weight = 1.25
@@ -70,42 +70,42 @@ local function check_pyzor(task)
             weight = 1.0
         elseif reported >= 1 and whitelisted == 0 then
             weight = 0.2
-		end
+        end
 
         if weight > 0 then
             task:insert_result(symbol_pyzor, weight, string.format("count=%d wl=%d", reported, whitelisted))
         end
-	end
+    end
 
-	local request = {
-		"CHECK\n",
-		task:get_content(),
-	}
+    local request = {
+        "CHECK\n",
+        task:get_content(),
+    }
 
-	logger.debugm(N, task, "querying pyzor")
+    logger.debugm(N, task, "querying pyzor")
 
-	tcp.request({
-		task = task,
-		host = opts["host"],
-		port = opts["port"],
-		shutdown = true,
-		data = request,
-		callback = cb,
-	})
+    tcp.request({
+        task = task,
+        host = opts["host"],
+        port = opts["port"],
+        shutdown = true,
+        data = request,
+        callback = cb,
+    })
 end
 
 if opts then
-	if opts.host then
-		cfg_host = opts.host
-	end
-	if opts.port then
-		cfg_port = opts.port
-	end
+    if opts.host then
+        cfg_host = opts.host
+    end
+    if opts.port then
+        cfg_port = opts.port
+    end
 
-	rspamd_config:register_symbol({
-		name = symbol_pyzor,
-		callback = check_pyzor,
-	})
+    rspamd_config:register_symbol({
+        name = symbol_pyzor,
+        callback = check_pyzor,
+    })
 else
-	logger.infox("%s module not configured", N)
+    logger.infox("%s module not configured", N)
 end
